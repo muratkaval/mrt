db06361de1bb5b111c916c6947c571f75b98eac2
Murat <muratrkaval@gmail.com>
2021-12-19 00:43:40 +0300

    deneme

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
-def hello(name):
-    print("Merhaba "+ name)
+"""veri1 = int(input("Birinci sayıyı giriniz: "))
+veri2 = int(input("İkinci sayıyı giriniz: "))
+toplam = veri1 + veri2
+ort = toplam/2
+print(f"toplam sayı {toplam},  ortalama ise {ort} ")
+"""
+import math
 
-hello(input("bir isim giriniz"))
+"""a = str(input("Birinci sayıyı giriniz: "))
+b = str(input("İkinci sayıyı giriniz: "))
+c = str(input("Üçüncü sayıyı giriniz: "))
+if (a>b) and (a>c):
+    print(f"{a} en büyük sayıdır")
+elif b>c and b>a:
+    print(f"{b} en büyük sayıdır.")
+else:
+    print(f"{c} en büyük sayıdır.")"""
 
-print("deneme")
+
+"""vize = int(input("Vize Sonucunuzu giriniz: "))
+final = int(input("Final Sonucunuzu giriniz: "))
+
+ort = (vize * 0.4) + (final * 0.6)
+a = int(ort)
+if a >50:
+    print(f" notunuz {a}, geçtiniz")
+else:
+    print(f"notunuz {a}, kaldınız")"""
+
+"""#birden yüze kadar olan tek sayıların toplamı
+toplam = 0
+for i in range(100):
+    toplam += i        #toplam = toplam + i
+print(f"1 den 100'e kadar olan sayıların toplamı {toplam} ")"""
+
+#1 den n sayısına kadar olan Tek sayıların toplamını bulmak
+n = int(input("Bir sayı giriniz: "))
+toplam = 0
+"""for i in range(n):
+    if i%2 ==1:
+        toplam += i
+print(f"Toplamda {toplam} kadar tek sayı vardır.")
+
+#2 yol
+for i in range(1,n,2):
+    toplam += i
+print(f"Toplamda {toplam} kadar tek sayı vardır.")"""
+
